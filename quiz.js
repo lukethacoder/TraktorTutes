d5524a8864b893204c1d9ab8b196c1bb2f111482
Luke Secomb <lukethadrummer@users.noreply.github.com>
2015-09-15 09:56:47 +1000

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,41 @@
+function submitAnswers() {
+	var total = 10;
+	var score = 0;
+	
+	// Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	var q6 = document.forms["quizForm"]["q6"].value;
+	var q7 = document.forms["quizForm"]["q7"].value;
+	var q8 = document.forms["quizForm"]["q8"].value;
+	var q9 = document.forms["quizForm"]["q9"].value;
+	var q10 = document.forms["quizForm"]["q10"].value;
+	
+	// Validation + Loop
+	for(i = 1; i <= total; i++) {
+		if(eval('q'+i) == null || eval('q'+i) == ''){
+		alert('You missed question '+ i);
+		return false;
+		}
+	}
+	
+	// Set Correct Answers
+	var answers = ["b","a","d","d","c","b","c","c","d","a"];
+	
+	// Check Answers
+	for(i = 1; i <= total;i++){
+		if(eval('q'+i) == answers[i - 1]){
+			score++;
+		}
+	}
+	
+	// Display Results
+	var results = document.getElementById('results');
+	results.innerHTML = '<h4> You scored <span>'+score+'</span> out of <span>'+total+'</span></h4>';
+	alert('You scored '+score+' out of '+total);
+	
+	return false;
+}
